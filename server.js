955d4ca23e1ca04f05a50146d6346c00c4a40edf
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-03-23 21:10:46 +0530

    test03

--- server.js
+++ server.js
@@ -18,7 +18,42 @@ app.get('/', (req, res) => {
     // res.senLdFile(__dirname + '/frontend/index.html');
 });
 app.get("/:room",(req,res)=>{
-    res.sendFile(__dirname + '/frontend/index.html')
+    io.on("connection", async (socket) => {
+        await connec()
+        console.log("A user connected");
+        socket.on("route",async (route,user)=>{
+            const check= await messanger.findOne({roomid:route})
+            console.log(check)
+            if (!check){
+                console.log("creating")
+                const mess=await messanger.create({
+                roomid:route,
+                messages:[
+                    {
+                        user:user,
+                        message:user+"joined",
+                        time:Date.now(),
+                        message_id:user+this.time
+                    }
+                ]
+            })
+            await mess.save()
+            console.log("dwqbk")
+        }
+        else{
+            console.log("hey i got you");
+        }
+        });
+        socket.on("connecting room",(route)=>{
+            socket.join(req.params.room)
+        })
+        socket.on("message", async (message,user) => {
+            io.emit("show", message,user);
+           await messanger.findOneAndUpdate({roomid:req.params.room},
+            {$push:{messages:{user:user,message:message,time:Date.now(),message_id:user+this.time}}})
+        });
+    });
+    
 })
 app.get("/data/:room",async (req,res)=>{
     const data=await messanger.findOne({roomid:req.params.room})
@@ -64,3 +99,4 @@ const PORT = 3033;
 http.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+app.listen(4040)
