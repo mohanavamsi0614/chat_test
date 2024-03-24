3d851bdcf164cd9d4c3b6132499c94e125ad45e8
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-03-24 11:59:21 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,16 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './comp/homme'
 import Chat from './comp/chat'
+import { useState } from 'react'
 // import test from ''
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/:room' element={<Chat/>}/>
+        <Route path='/:room' element={<Chat />}/>
       </Routes>
     </>
   )
 }
-
 export default App;
--- server.js
+++ server.js
@@ -1,59 +1,26 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const { Server } = require('socket.io');
+// const { Server } = require('socket.io');
+// const cors = require('cors'); 
+const io = require('socket.io')(http); 
 const cors = require('cors'); 
 const {messanger}=require("./db")
 const {connec}=require("./db");
-const io = new Server(http, {
-    cors: {
-        origin: '*', 
-        methods: ['GET', 'POST',"PUT",],
-        credentials: true
-    }
-});
+// const io = new Server(http, {
+//     cors: {
+//         origin: '*', 
+//         methods: ['GET', 'POST',"PUT",],
+//         credentials: true
+//     }
+// });
 app.use(cors());
 app.get('/', (req, res) => {
     res.send("kjwqhk")
     // res.senLdFile(__dirname + '/frontend/index.html');
 });
 app.get("/:room",(req,res)=>{
-    io.on("connection", async (socket) => {
-        await connec()
-        console.log("A user connected");
-        socket.on("route",async (route,user)=>{
-            const check= await messanger.findOne({roomid:route})
-            console.log(check)
-            if (!check){
-                console.log("creating")
-                const mess=await messanger.create({
-                roomid:route,
-                messages:[
-                    {
-                        user:user,
-                        message:user+"joined",
-                        time:Date.now(),
-                        message_id:user+this.time
-                    }
-                ]
-            })
-            await mess.save()
-            console.log("dwqbk")
-        }
-        else{
-            console.log("hey i got you");
-        }
-        });
-        socket.on("connecting room",(route)=>{
-            socket.join(req.params.room)
-        })
-        socket.on("message", async (message,user) => {
-            io.emit("show", message,user);
-           await messanger.findOneAndUpdate({roomid:req.params.room},
-            {$push:{messages:{user:user,message:message,time:Date.now(),message_id:user+this.time}}})
-        });
-    });
-    
+    res.sendFile(__dirname + '/frontend/index.html')
 })
 app.get("/data/:room",async (req,res)=>{
     const data=await messanger.findOne({roomid:req.params.room})
@@ -99,4 +66,3 @@ const PORT = 3033;
 http.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
-app.listen(4040)
