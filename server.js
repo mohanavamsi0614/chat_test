16cf10bf2c3225f865339ad9a988f0d0b55d53e0
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-03-27 12:09:10 +0530

    d

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cors({
     origin: 'https://chayo-01.netlify.app/', 
     methods: ['GET', 'POST'],
-    a
+
   }));
 connec().then(()=>{console.log(" connected");})
   
